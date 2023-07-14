60ab27539f3c206a9b6e46dbd5698020b6524e80
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-07-14 04:39:43 -0300

    Se borraron las clave publicas

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Checkout from "./components/Checkout/Checkout";
 
 
 function App() {
+  console.log(process.env.REACT_APP_API_KEY)
   return (
     <>
       <BrowserRouter>
--- src/components/service/firebase/firebaseConfig.js
+++ src/components/service/firebase/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLTDZXLAgquTwhVR5fuVPNIBxIkCwX4_M",
-  authDomain: "musimundo-41c8f.firebaseapp.com",
-  projectId: "musimundo-41c8f",
-  storageBucket: "musimundo-41c8f.appspot.com",
-  messagingSenderId: "634668376097",
-  appId: "1:634668376097:web:dd4351b59cc0df93a68360",
-  measurementId: "G-ZESVS9H402"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 // Initialize Firebase
